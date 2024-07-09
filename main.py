1a7ca3323be0a34f3f6a277f58c8fe83ccc870b4
ryotakata <w.tkt.r.8700@gmail.com>
2024-07-10 05:51:39 +0900

    [mod] main.py, searching *.md files recursively

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
+from logging import getLogger
+from os import makedirs as makedirs
+from os.path import abspath as abspath
+from os.path import dirname as dirname
+from os.path import exists as path_exists
+from os.path import join as path_join
+from os.path import relpath as relpath
+from posixpath import abspath
 import argparse
 import logging
-from logging import getLogger
-parser = argparse.ArgumentParser(description='Set logging level')
+parser = argparse.ArgumentParser(description='Inserting Anchor Link')
+parser.add_argument('--input-dir', dest='input_dir', type=str, default='input-github', help='Input Directory Path')
+parser.add_argument('--output-dir', dest='output_dir', type=str, default='output', help='Output Directory Path')
 parser.add_argument('--log-level', dest='log_level', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
                     default='WARNING', help='Logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)')
 args = parser.parse_args()
@@ -11,20 +20,24 @@ numeric_level = getattr(logging, args.log_level, None)
 if not isinstance(numeric_level, int):
     raise ValueError('Invalid log level: %s' % args.log_level)
 logging.basicConfig(level=numeric_level)
+input_dir = abspath(args.input_dir)
+output_dir = abspath(args.output_dir)
 
 class Repository:
-    def __init__(self):
+    def __init__(self, input_dir, output_dir):
         import glob
-        md_file_paths = glob.glob('*.md')
+        pathname = path_join(input_dir, '**', '*.md')
+        md_file_paths = glob.glob(pathname, recursive=True)
         self.md_files = [MarkDownFile(f) for f in md_file_paths]
         logger.info("Markdown files, which this script found, are:")
         for f in md_file_paths:
             logger.info(" - " + f)
+        self.input_dir = input_dir    
+        self.output_dir = output_dir    
         return
     def generate(self, output_dir='output'):
-        import os
-        if not os.path.exists(output_dir):
-            os.mkdir(output_dir)
+        if not path_exists(output_dir):
+            makedirs(output_dir)
         else:
             logger.warning("Directory {} is already exist.".format(output_dir))
         [ f.generate_anchor() for f in self.md_files]
@@ -32,7 +45,9 @@ class Repository:
         all_tech_terms = [ t for f in self.md_files for t in f.tech_terms]
         for t in sorted(all_tech_terms, reverse=True):
             [ f.generate_link(t) for f in self.md_files]
-        [ f.generate_output_md_file(output_dir) for f in self.md_files]
+        input_dir = self.input_dir
+        output_dir = self.output_dir
+        [ f.generate_output_md_file(input_dir, output_dir) for f in self.md_files]
         return
 
 import re
@@ -89,11 +104,17 @@ class MarkDownFile:
         self.text = re.sub(pattern, entry_word_with_link, self.text, flags=re.MULTILINE)
 
         return
-    def generate_output_md_file(self, output_dir):
-        output_path = output_dir + '/' + self.path
+    def generate_output_md_file(self, input_dir, output_dir):
+        output_path = self._replase_base_dir(self.path, input_dir, output_dir)
+        dir_ = dirname(output_path)
+        if not path_exists(dir_):
+            makedirs(dir_)
         with open(output_path, "w") as f:
             f.write(self.text)
         return
+    def _replase_base_dir (self, target_path, old_base, new_base):
+        relative_path = relpath(target_path, old_base)
+        return path_join(new_base, relative_path)
 
 class TechTerm:
     def __init__(self, anchor, entry_word, md_file):
@@ -106,5 +127,5 @@ class TechTerm:
             return NotImplemented
         return len(self.entry_word) < len(other.entry_word)
 if __name__ == "__main__": 
-    r = Repository()
+    r = Repository(input_dir, output_dir)
     r.generate()
