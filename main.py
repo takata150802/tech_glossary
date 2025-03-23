1e776bb9793167d8c711cf46a9e828baa7b7ce75
ryotakata <w.tkt.r.8700@gmail.com>
2025-03-23 21:26:41 +0900

    [mod] main.py, using markdown-it-py(markdown parser) and mdformat(formatter)

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+from markdown_it import MarkdownIt
+from markdown_it.token import Token
+from markdown_it.tree import SyntaxTreeNode 
+from mdformat.renderer import MDRenderer
+import base64
 from logging import getLogger
 from os import makedirs as makedirs
 from os.path import abspath as abspath
@@ -6,7 +11,7 @@ from os.path import dirname as dirname
 from os.path import exists as path_exists
 from os.path import join as path_join
 from os.path import relpath as relpath
-from posixpath import abspath
+from os.path import commonpath as commonpath
 import argparse
 import logging
 parser = argparse.ArgumentParser(description='Inserting Anchor Link')
@@ -24,39 +29,72 @@ input_dir = abspath(args.input_dir)
 output_dir = abspath(args.output_dir)
 
 class Repository:
-    def __init__(self, input_dir, output_dir):
+    def __init__(self, input_dir):
+        self.input_dir = input_dir
         import glob
         pathname = path_join(input_dir, '**', '*.md')
         md_file_paths = glob.glob(pathname, recursive=True)
-        self.md_files = [MarkDownFile(f) for f in md_file_paths]
+        md_file_relpaths = [relpath(p, input_dir) for p in md_file_paths]
+        self.md_files = [MarkDownFile(self, p) for p in md_file_relpaths]
         logger.info("Markdown files, which this script found, are:")
         for f in md_file_paths:
             logger.info(" - " + f)
-        self.input_dir = input_dir    
-        self.output_dir = output_dir    
         return
-    def generate(self, output_dir='output'):
+    def generate(self, output_dir):
+        self.output_dir = output_dir
         if not path_exists(output_dir):
             makedirs(output_dir)
         else:
             logger.warning("Directory {} is already exist.".format(output_dir))
-        [ f.generate_anchor() for f in self.md_files]
-        # [NOTE]: 全markdown fileの全Tech Termを文字列長降順でソートしてループする
-        all_tech_terms = [ t for f in self.md_files for t in f.tech_terms]
-        for t in sorted(all_tech_terms, reverse=True):
-            [ f.generate_link(t) for f in self.md_files]
-        input_dir = self.input_dir
-        output_dir = self.output_dir
-        [ f.generate_output_md_file(input_dir, output_dir) for f in self.md_files]
+        [ f.generate_link() for f in self.md_files]
+        [ f.generate_output_md_file() for f in self.md_files]
         return
 
 import re
 re_url = re.compile(r'<!-- 記事URL:(.*) -->')
-re_term = re.compile(r'(#+) (.*) <!-- entry_word_and_anchor:(\w*) -->')
-re_gen_achor = re.compile(r'<!-- entry_word_and_anchor:(\w*) -->')
 class MarkDownFile:
-    def __init__(self, path):
-        self.path = path
+    def __init__(self, klass_repository, relpath):
+        self.relpath = relpath
+        self.repo = klass_repository
+        self.url = self._re_match_url()
+        self.tech_terms, self.tokens, self.node = self._generate_tech_terms()
+        return
+    def _generate_tech_terms(self):
+        # [NOTE]: TechTermのentry_wordとanchorを取得
+        path = self.get_input_file_path()
+        with open(path, 'r') as f:
+            text = f.read()
+        md = MarkdownIt("commonmark")
+        tokens = md.parse(text)
+        node = SyntaxTreeNode(tokens)
+        tech_terms = []
+        for i in [ iii for iii in node.walk() 
+                       if iii.type == 'heading' 
+                       and  iii.nester_tokens.opening.tag == 'h2']:
+            for inline_node  in [ iii for iii in i.walk() if iii.type == 'inline']:
+                assert all([c.map == None for c in inline_node.children])
+                anchor = base64.b64encode(inline_node.content.encode('utf-8')).decode('utf-8')
+                atag_tokens_content = '<a id="' + anchor + '">'
+                atag_tokens = [
+                        Token(type='html_inline', tag='', nesting=0, attrs={}, map=None, level=0, children=None, content=atag_tokens_content, markup='', info='', meta={}, block=False, hidden=False),
+                        Token(type='html_inline', tag='', nesting=0, attrs={}, map=None, level=0, children=None, content='</a>', markup='', info='', meta={}, block=False, hidden=False),
+                        ]
+                ### NOTE: The "inline_node.token.children" are Token(s) in the same file of the markdown file
+                inline_node.token.children.extend(atag_tokens)
+                for c in [ccc for ccc in inline_node.children if ccc.type == 'text']:
+                    header_h2_text = c.content
+                    for entry_word in [ i.strip() for i in header_h2_text.split('|')]:
+                        ### NOTE: 同一ファイル内で同じ見出し語(h2)はNG
+                        assert entry_word not in [t.entry_word for t in tech_terms], entry_word
+                        t = TechTerm(anchor, entry_word, self)
+                        tech_terms.append(t)
+        tokens = node.to_tokens()
+        ### NOTE: 見出し語の文字列長い順に並び替え
+        tech_terms.sort(key=lambda x: len(x.entry_word), reverse=True)
+        node = SyntaxTreeNode(tokens)
+        return tech_terms, tokens, node 
+    def _re_match_url(self):
+        path = self.get_input_file_path()
         with open(path, 'r') as f:
             self.text = f.read()
         # [NOTE]: 記事URL(=https:~ ) の部分だけ後方参照して取り出す
@@ -64,57 +102,85 @@ class MarkDownFile:
         if len(ret) != 1:
             logger.fatal('Markdown File {} should have only one url.'.format(path))
             logger.fatal([str(ret) for ret in ret])
-            self.url = None
+            return None
         else:
-            self.url = ret[0]
             logger.debug(ret[0])
-        # [NOTE]: TechTermのentry_wordとanchorを取得
-        ret = re.findall(re_term, self.text)
-        tech_terms = []
-        for tech_term_and_anchor in ret:
-            entry_words = tech_term_and_anchor[1]
-            anchor = tech_term_and_anchor[2]
-            logger.debug(anchor)
-            logger.debug(entry_words)
-            for entry_word in [ i.strip() for i in entry_words.split('|')]:
-                t = TechTerm(anchor, entry_word, self)
-                tech_terms.append(t)
-        self.tech_terms = tech_terms
+            return ret[0]
+    def get_input_file_path(self):
+        return path_join(self.repo.input_dir, self.relpath)
+    def get_output_file_path(self):
+        return path_join(self.repo.output_dir, self.relpath)
+    def generate_link(self):
+        ### NOTE: 他のMarkDownFileをソートする。基準は、自身の.relpathとの共通部分の長さ
+        self.repo.md_files.sort(key=lambda x: commonpath_length(self.relpath, x.relpath), reverse=True)
+        all_tech_terms = [ t for f in self.repo.md_files for t in f.tech_terms]
+        ### NOTE: markdown_it のSyntaxTreeNodeのwalk()メソッドの改造版
+        ###       無視すべきnodeとその配下のnodeをskipする
+        from typing import TypeVar
+        from collections.abc import Generator
+        _NodeType = TypeVar("_NodeType", bound="SyntaxTreeNode")
+        def my_node_walk(self: _NodeType) -> Generator[_NodeType, None, None]:
+            if self.type in ("heading", "link", "html_inline", "html_block", "code_inline"):
+                return
+            yield self
+            for child in self.children:
+                yield from child.my_node_walk()
+        setattr(SyntaxTreeNode, my_node_walk.__name__, my_node_walk)
+        for inline_node  in [ i for i in self.node.my_node_walk() if i.type == 'inline']:
+            ### NOTE: The "inline_node.token.children" are Token(s) in the same file of the markdown file
+            assert all([c.map == None for c in inline_node.children])
+            for tech_term in all_tech_terms:
+                entry_word = tech_term.entry_word
+                anchor = tech_term.anchor
+                url = tech_term.md_file.url
+                # link_token_content = '<a href="' + url + '&anchor=' + anchor + '">'
+                link_token_content = '<a href="' + url + anchor + '">'
+                while True:
+                    token_replaced_and_retry  = False
+                    new_list_siblings  = []
+                    inside_atag = False
+                    for s in inline_node.token.children:
+                        if s.type == "html_inline" and s.content.startswith('<a '):
+                            inside_atag = True
+                        elif s.type == "html_inline" and s.content == '</a>':
+                            inside_atag = False
+                        else:
+                            pass
+                        if inside_atag:
+                            new_list_siblings.append(s)
+                        elif entry_word in s.content:
+                            token_replaced_and_retry = True
+                            index = s.content.index(entry_word)
+                            first_part = s.content[:index]
+                            second_part = s.content[index + len(entry_word):]
+                            replaced_tokens = [
+                                    Token(type='text', tag='', nesting=0, attrs={}, map=None, level=0, children=None, content=first_part, markup='', info='', meta={}, block=False, hidden=False),
+                                    Token(type='html_inline', tag='', nesting=0, attrs={}, map=None, level=0, children=None, content=link_token_content, markup='', info='', meta={}, block=False, hidden=False),
+                                    Token(type='text', tag='', nesting=0, attrs={}, map=None, level=0, children=None, content=entry_word, markup='', info='', meta={}, block=False, hidden=False),
+                                    Token(type='html_inline', tag='', nesting=0, attrs={}, map=None, level=0, children=None, content='</a>', markup='', info='', meta={}, block=False, hidden=False),
+                                    Token(type='text', tag='', nesting=0, attrs={}, map=None, level=0, children=None, content=second_part,  markup='', info='', meta={}, block=False, hidden=False),
+                                    ]
+                            new_list_siblings.extend(replaced_tokens)
+                        else:
+                            new_list_siblings.append(s)
+                    inline_node.token.children = new_list_siblings
+                    if token_replaced_and_retry:
+                        continue
+                    else:
+                        break
+        self.tokens = self.node.to_tokens()
+        self.node = SyntaxTreeNode(self.tokens)
         return
-    def generate_anchor(self):
-        term_with_achor = r'<a id="\3"></a>\n'
-        term_with_achor += r'\1 \2 '
-        term_with_achor += r'<!-- entry_word_and_anchor:\3 -->'
-        self.text = re.sub(re_term, 
-                           term_with_achor,
-                           self.text)
-        return
-    def generate_link(self, tech_term):
-        entry_word = tech_term.entry_word
-        entry_word_with_link = r'\1'
-        entry_word_with_link += r'<a href="' + tech_term.md_file.url
-        entry_word_with_link += tech_term.anchor
-        entry_word_with_link += r'"> ' + tech_term.entry_word
-        entry_word_with_link += r' </a>'
-
-        pattern = r'^(?!#)(?!.*<!-- substituteinhibit -->.*$)(.*){0}(?!(?!.*<a).*<\/a>)(?!(?!.*<!--).*-->)'.format(entry_word)
-        re_sub = re.compile(pattern)
-        logger.debug(pattern)
-        logger.debug(re.search(pattern, self.text, flags=re.MULTILINE))
-        self.text = re.sub(pattern, entry_word_with_link, self.text, flags=re.MULTILINE)
-
-        return
-    def generate_output_md_file(self, input_dir, output_dir):
-        output_path = self._replase_base_dir(self.path, input_dir, output_dir)
+    def generate_output_md_file(self):
+        renderer = MDRenderer()
+        output_markdown = renderer.render(self.tokens, options={}, env={})
+        output_path = self.get_output_file_path()
         dir_ = dirname(output_path)
         if not path_exists(dir_):
             makedirs(dir_)
         with open(output_path, "w") as f:
-            f.write(self.text)
+            f.write(output_markdown)
         return
-    def _replase_base_dir (self, target_path, old_base, new_base):
-        relative_path = relpath(target_path, old_base)
-        return path_join(new_base, relative_path)
 
 class TechTerm:
     def __init__(self, anchor, entry_word, md_file):
@@ -126,6 +192,12 @@ class TechTerm:
         if not isinstance(other, TechTerm):
             return NotImplemented
         return len(self.entry_word) < len(other.entry_word)
+
+def commonpath_length(base_path: str, target_path: str) -> int:
+    # パスを分割して比較
+    common_ = commonpath([base_path, target_path])
+    return len(common_)
+
 if __name__ == "__main__": 
-    r = Repository(input_dir, output_dir)
-    r.generate()
+    r = Repository(input_dir)
+    r.generate(output_dir)
